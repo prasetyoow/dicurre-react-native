ce6b59b9fd98736f8402c94156313631f76e1db5
prasetyoow <praztamer@gmail.com>
2022-09-08 20:49:38 +0700

    add backend/axios abstraction

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,13 +1,17 @@
-import Axios from 'axios';
+import axios from 'axios';
+
+const url = 'http://192.168.1.3:8888/';
+// const url = 'http://localhost:8888';
+// const url = 'https://fazzpay.herokuapp.com/';
 
 const http = token => {
   const headers = {};
   if (token) {
     headers.authorization = `Bearer ${token}`;
   }
-  return Axios.create({
+  return axios.create({
     headers,
-    baseURL: 'http://192.168.1.4:8888',
+    baseURL: url,
     // baseURL: process.env.REACT_NATIVE_APP_URL,
   });
 };
