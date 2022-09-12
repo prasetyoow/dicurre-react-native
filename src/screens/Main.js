a398af5510c85016ff69f26af2e4543d4ade815e
prasetyoow <praztamer@gmail.com>
2022-09-12 17:30:21 +0700

    fixed navigator

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -5,20 +5,21 @@ import {NavigationContainer} from '@react-navigation/native';
 // Screen
 import LoginScreen from '../screens/Login';
 // import Home from './Dashboard/Home';
-import HomeTabStack from './HomeTabStack';
+import HomeStack from './HomeStack';
+// import HomeTabStack from './HomeTabStack';
 import SignupScreen from '../screens/Signup';
 import CreatePinScreen from '../screens/CreatePIN/CreatePinScreen';
 import CreatePinSuccess from '../screens/CreatePIN/PinSuccessScreen';
 import ResetPassword from '../screens/ResetPassword/ResetPassword';
 import NewPassword from '../screens/ResetPassword/NewPassword';
-import PersonalInfo from '../screens/Dashboard/Profile/PersonalInfo';
-import ChangePassword from '../screens/Dashboard/Profile/ChangePassword';
-import ChangePIN from '../screens/Dashboard/Profile/ChangePIN';
-import InputAmount from '../screens/Dashboard/Transaction/InputAmount';
-import Confirmation from '../screens/Dashboard/Transaction/Confirmation';
-import EnterPIN from '../screens/Dashboard/Transaction/EnterPIN';
-import TransferSuccess from '../screens/Dashboard/Transaction/TransferSuccess';
-import Details from '../screens/Dashboard/History/Details';
+// import PersonalInfo from '../screens/Dashboard/Profile/PersonalInfo';
+// import ChangePassword from '../screens/Dashboard/Profile/ChangePassword';
+// import ChangePIN from '../screens/Dashboard/Profile/ChangePIN';
+// import InputAmount from '../screens/Dashboard/Transaction/InputAmount';
+// import Confirmation from '../screens/Dashboard/Transaction/Confirmation';
+// import EnterPIN from '../screens/Dashboard/Transaction/EnterPIN';
+// import TransferSuccess from '../screens/Dashboard/Transaction/TransferSuccess';
+// import Details from '../screens/Dashboard/History/Details';
 
 import {useSelector} from 'react-redux';
 
@@ -33,8 +34,8 @@ const Main = () => {
           {token ? (
             <Stack.Screen
               options={{headerShown: false}}
-              name="HomeTabStack"
-              component={HomeTabStack}
+              name="HomeStack"
+              component={HomeStack}
             />
           ) : (
             <>
@@ -67,51 +68,6 @@ const Main = () => {
                 options={{headerShown: false}}
                 name="NewPassword"
                 component={NewPassword}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="HomeTab"
-                component={HomeTabStack}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="PersonalInfo"
-                component={PersonalInfo}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="ChangePassword"
-                component={ChangePassword}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="ChangePIN"
-                component={ChangePIN}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="InputAmount"
-                component={InputAmount}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="Confirmation"
-                component={Confirmation}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="EnterPIN"
-                component={EnterPIN}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="TransferSuccess"
-                component={TransferSuccess}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="Details"
-                component={Details}
               />
             </>
           )}
